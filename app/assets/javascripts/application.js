e3198f54700633d84e1d839122da8047c4a368a0
ryan chew <ryan.layosh@gmail.com>
2014-02-07 11:48:55 +0800

    foundation fixes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,5 +10,8 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
+//= require foundation
 //= require_tree .
+//= require modernizr
 
